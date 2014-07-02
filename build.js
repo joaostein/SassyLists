1c811f4651d75d265e00fea313cce1421f7afcc8
Hugo Giraudel <hugo.giraudel@gmail.com>
2014-07-02 18:35:39 +0200

    Added version to navbar

--- build.js
+++ build.js
@@ -9,14 +9,15 @@ var version = "2.1.1";
 
 sassdoc.parse(__dirname + '/gems/ruby/2.0.0/gems/SassyLists-' + version + '/stylesheets').then(function (items) {
   var templat_doc    = swig.compileFile(__dirname + '/templates/documentation.html.swig');
+  var data = { 'version': version, 'data': items };
 
-  fs.writeFile('documentation.html', templat_doc({ 'data': items }), function (err) {
+  fs.writeFile('documentation.html', templat_doc(data), function (err) {
     if (err) throw err;
     console.log('Documentation correctly generated!');
 
     var template_index = swig.compileFile(__dirname + '/templates/index.html.swig');
 
-    fs.writeFile('index.html', template_index({ 'version': version, 'data': items }), function (err) {
+    fs.writeFile('index.html', template_index(data), function (err) {
       if (err) throw err;
       console.log('Index correctly generated!');
     });
